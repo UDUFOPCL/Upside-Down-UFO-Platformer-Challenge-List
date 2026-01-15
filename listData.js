fd9f089d4641b30c4089ee019518d06d3e50536d
UDUFOPCL <udufopcl@gmail.com>
2026-01-15 08:46:59 -0500

    Clean code and easier modificaiton

--- listData.js
+++ listData.js
@@ -1,274 +1,137 @@
 const data = {
-    1: [
-        "name" = "FATE OF THE STARS",
-        "ID" = 114176600,
-        "creator" = "ZiroTrans",
-        "verifier" = "Astatine16",
-        "recorder" = "Astatine16",
-        "time" = "1:20.550"
-    ],
-    2: [
-        "name" = "BrambleIfItWasGood",
-        "ID" = 114998675,
-        "creator" = "ashfalcons44",
-        "verifier" = "ashfalcons44",
-        "recorder" = "ashfalcons44",
-        "time" = "13.725"
-    ],
-    3: [
-        "name" = "Lament",
-        "ID" = 114988101,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "22.520"
-    ],
-    4: [
-        "name" = "BlUDUFObath",
-        "ID" = 114965788,
-        "creator" = "rush",
-        "verifier" = "rush",
-        "recorder" = "rush",
-        "time" = "2:45.383"
-    ],
-    5: [
-        "name" = "Fever",
-        "ID" = 114963741,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "17.400"
-    ],
-    6: [
-        "name" = "Suffer",
-        "ID" = 111863467,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "21.912"
-    ],
-    7: [
-        "name" = "agony",
-        "ID" = 114960048,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "19.166"
-    ],
-    8: [
-        "name" = "mangoa",
-        "ID" = 114952534,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "MyCat223",
-        "time" = "15.812"
-    ],
-    9: [
-        "name" = "vsc but ufo",
-        "ID" = 114984854,
-        "creator" = "CoinvesselGD",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "35.791"
-    ],
-    10: [
-        "name" = "Mycat sigma boy chal",
-        "ID" = 114984412,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "17.754"
-    ],
-    11: [
-        "name" = "spam challenge 1",
-        "ID" = 114952847,
-        "creator" = "tenkuyuhh",
-        "verifier" = "tenkuyuhh",
-        "recorder" = "tenkuyuhh",
-        "time" = "20.091"
-    ],
-    12: [
-        "name" = "average udufopcl level",
-        "ID" = 114935113,
-        "creator" = "MyCat223",
-        "verifier" = "MyCat223",
-        "recorder" = "MyCat223",
-        "time" = "37.762"
-    ],
-    13: [
-        "name" = "zimbabwe",
-        "ID" = 115127631,
-        "creator" = "TheNewCodex",
-        "verifier" = "TheNewCodex",
-        "recorder" = "TheNewCodex",
-        "time" = "9.166"
-    ],
-    14: [
-        "name" = "le funky ufo level",
-        "ID" = 114952921,
-        "creator" = "ItsNoobe",
-        "verifier" = "ItsNoobe",
-        "recorder" = "rush",
-        "time" = "29.770"
-    ],
-    15: [
-        "name" = "TOOMDUDUFOIGDPCLBM",
-        "ID" = 114955327,
-        "creator" = "Codex",
-        "verifier" = "Codex",
-        "recorder" = "Codex",
-        "time" = "9.083"
-    ]
-};
-
-[
-    //1
-    {
-        "name": "FATE OF THE STARS",
-        "ID": 114176600,
-        "creator": "ZiroTrans",
-        "verifier": "Astatine16",
-        "recorder": "Astatine16",
-        "time": "1:20.550"
+    1 : {
+        name : 'FATE OF THE STARS',
+        image : 'Images/fateStars.webp',
+        ID : 114176600,
+        creator : 'ZiroTrans',
+        verifier : 'Astatine16',
+        recorder : 'Astatine16',
+        time : '1:20.550'
     },
-
-    //2
-    {
-        "name": "BrambleIfItWasGood",
-        "ID": 114998675,
-        "creator": "ashfalcons44",
-        "verifier": "ashfalcons44",
-        "recorder": "ashfalcons44",
-        "time": "13.725"
+    2: {
+        name : 'BrambleIfItWasGood',
+        image : 'Images/barmbleGood.webp',
+        ID : 114998675,
+        creator : 'ashfalcons44',
+        verifier : 'ashfalcons44',
+        recorder : 'ashfalcons44',
+        time : '13.725'
     },
-
-    //3
-    {
-        "name": "Lament",
-        "ID": 114988101,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "22.520"
+    3: {
+        name : 'Lament',
+        image : 'Images/lament.webp',
+        ID : 114988101,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '22.520'
     },
-
-    //4
-    { 
-        "name": "BlUDUFObath",
-        "ID": 114965788,
-        "creator": "rush",
-        "verifier": "rush",
-        "recorder": "rush",
-        "time": "2:45.383"
+    4: {
+        name : 'BlUDUFObath',
+        image : 'Images/bath.webp',
+        ID : 114965788,
+        creator : 'rush',
+        verifier : 'rush',
+        recorder : 'rush',
+        time : '2:45.383'
     },
-
-    //5
-    {
-        "name": "Fever",
-        "ID": 114963741,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "17.400"
+    5: {
+        name : 'Fever',
+        image : 'Images/fever.webp',
+        ID : 114963741,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '17.400'
     },
-
-    //6
-    {
-        "name": "Suffer",
-        "ID": 111863467,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "21.912"
+    6: {
+        name : 'Suffer',
+        image : 'Images/suffer.webp',
+        ID : 111863467,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '21.912'
     },
-
-    //7
-    {
-        "name": "agony",
-        "ID": 114960048,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "19.166"
+    7: {
+        name : 'agony',
+        image : 'Images/agony.webp',
+        ID : 114960048,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '19.166'
     },
-
-    //8
-    {
-        "name": "mangoa",
-        "ID": 114952534,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "MyCat223",
-        "time": "15.812"
+    8: {
+        name : 'mangoa',
+        image : 'Images/mangoa.webp',
+        ID : 114952534,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'MyCat223',
+        time : '15.812'
     },
-
-    //9
-    {
-        "name": "vsc but ufo",
-        "ID": 114984854,
-        "creator": "CoinvesselGD",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "35.791"
+    9: {
+        name : 'vsc but ufo',
+        image : 'Images/vscUfo.webp',
+        ID : 114984854,
+        creator : 'CoinvesselGD',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '35.791'
     },
-
-    //10
-    {
-        "name": "Mycat sigma boy chal",
-        "ID": 114984412,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "17.754"
+    10: {
+        name : 'Mycat sigma boy chal',
+        image : 'Images/sigmaBoy.webp',
+        ID : 114984412,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '17.754'
     },
-
-    //11
-    {
-        "name": "spam challenge 1",
-        "ID": 114952847,
-        "creator": "tenkuyuhh",
-        "verifier": "tenkuyuhh",
-        "recorder": "tenkuyuhh",
-        "time": "20.091"
+    11: {
+        name : 'spam challenge 1',
+        image : 'Images/spam1.webp',
+        ID : 114952847,
+        creator : 'tenkuyuhh',
+        verifier : 'tenkuyuhh',
+        recorder : 'tenkuyuhh',
+        time : '20.091'
     },
-
-    //12
-    {
-        "name": "average udufopcl level",
-        "ID": 114935113,
-        "creator": "MyCat223",
-        "verifier": "MyCat223",
-        "recorder": "MyCat223",
-        "time": "37.762"
+    12: {
+        name : 'average udufopcl level',
+        image : 'Images/average.webp',
+        ID : 114935113,
+        creator : 'MyCat223',
+        verifier : 'MyCat223',
+        recorder : 'MyCat223',
+        time : '37.762'
     },
-
-    //13
-    {
-        "name": "zimbabwe",
-        "ID": 115127631,
-        "creator": "TheNewCodex",
-        "verifier": "TheNewCodex",
-        "recorder": "TheNewCodex",
-        "time": "9.166"
+    13: {
+        name : 'zimbabwe',
+        image : 'Images/zimbabwe.webp',
+        ID : 115127631,
+        creator : 'TheNewCodex',
+        verifier : 'TheNewCodex',
+        recorder : 'TheNewCodex',
+        time : '9.166'
     },
-
-    //14
-    {
-        "name": "le funky ufo level",
-        "ID": 114952921,
-        "creator": "ItsNoobe",
-        "verifier": "ItsNoobe",
-        "recorder": "rush",
-        "time": "29.770"
+    14: {
+        name : 'le funky ufo level',
+        image : 'Images/leFunky.webp',
+        ID : 114952921,
+        creator : 'ItsNoobe',
+        verifier : 'ItsNoobe',
+        recorder : 'rush',
+        time : '29.770'
     },
-
-    //15
-    {
-        "name": "TOOMDUDUFOIGDPCLBM",
-        "ID": 114955327,
-        "creator": "Codex",
-        "verifier": "Codex",
-        "recorder": "Codex",
-        "time": "9.083"
+    15: {
+        name : 'TOOMDUDUFOIGDPCLBM',
+        image : 'Images/toomdud.webp',
+        ID : 114955327,
+        creator : 'Codex',
+        verifier : 'Codex',
+        recorder : 'Codex',
+        time : '9.083'
     }
-]
+};
