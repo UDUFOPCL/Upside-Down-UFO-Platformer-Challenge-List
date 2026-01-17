6c8800e7903e1f60d1f8b75a3b6cf9de37fecc84
UDUFOPCL <udufopcl@gmail.com>
2026-01-16 23:22:47 -0500

    Re-Release & New Levels

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+// Stores the length of the list from listData.js as a variable so code is easier to read and edit
 length = Object.keys(data).length;
+
+// Changes the info on the website to display the actual stuff
+// This works by running a loop that cycles through all the data to display each piece of info
+// This shouldn't need changed unless you want to add more things to dispaly for a level
 
 for (let i = 1; i < length + 1; i++) {
     document.getElementById('name'+i).innerHTML = data[i]['name']
     document.getElementById('image'+i).src = data[i]['image']
     document.getElementById('creator'+i).innerHTML = data[i]['creator'];
     document.getElementById('time'+i).innerHTML = data[i]['recorder'] + ' - ' + data[i]['time'];
-    
 }
 
+//Copies the level ID to your clipboard and shows the popup
 function copy(ID) {
     navigator.clipboard.writeText(ID)
     document.getElementById('alertText').innerHTML = ('Copied ID: ' + ID);
     document.getElementById('alert').classList.remove('hide')
     console.log('ID Showing')
     setTimeout(show, 100)
-    
-
 }
+
+//Hides the "ID Copied" popup
 function show() {
     document.getElementById('alert').classList.add('hide')
     console.log('ID Vanishing')
--- listData.js
+++ listData.js
@@ -1,137 +1,207 @@
+/*
+All info in the list for each level
+
+Level data includes:
+    name : Name of the level in-game
+    image : File path of the level image
+    ID : ID for the level in-game
+    creator : Name of the player who created the level
+    verifier : Name of the player who verified the level
+    recorder : Name of the player who currently holds the fastest completion for the level
+    time : The time for the fastest completion of the level
+
+Level Template:
+
+    # : {
+        name : '',
+        image : 'images/',
+        ID : ,
+        creator : '',
+        verifier : '',
+        recorder : '',
+        time : 'X:XX.XXX'
+    },
+
+*/
 const data = {
     1 : {
-        name : 'FATE OF THE STARS',
-        image : 'Images/fateStars.webp',
-        ID : 114176600,
-        creator : 'ZiroTrans',
+        name : 'FALLEN ANGEL',
+        image : 'images/fallenAngel.webp',
+        ID : 131366052,
+        creator : 'Astatine16',
         verifier : 'Astatine16',
         recorder : 'Astatine16',
-        time : '1:20.550'
+        time : '18.599'
     },
-    2: {
+    2 : {
         name : 'BrambleIfItWasGood',
-        image : 'Images/barmbleGood.webp',
+        image : 'images/barmbleGood.webp',
         ID : 114998675,
         creator : 'ashfalcons44',
         verifier : 'ashfalcons44',
         recorder : 'ashfalcons44',
         time : '13.725'
     },
-    3: {
+    3 : {
+        name : 'noobedufopc ultra',
+        image : 'images/noobedufopcUltra.webp',
+        ID : 131546166,
+        creator : 'ItsNoobe',
+        verifier : 'ItsNoobe',
+        recorder : 'ItsNoobe',
+        time : '39.645'
+    },
+    4 : {
         name : 'Lament',
-        image : 'Images/lament.webp',
+        image : 'images/lament.webp',
         ID : 114988101,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '22.520'
     },
-    4: {
+    5 : {
         name : 'BlUDUFObath',
-        image : 'Images/bath.webp',
+        image : 'images/bath.webp',
         ID : 114965788,
         creator : 'rush',
         verifier : 'rush',
         recorder : 'rush',
         time : '2:45.383'
     },
-    5: {
+    6 : {
+        name : 'run from emoji9',
+        image : 'images/runFromEmoji9.webp',
+        ID : 119771687,
+        creator : 'ItsNoobe',
+        verifier : 'ItsNoobe',
+        recorder : 'ItsNoobe',
+        time : '19.066'
+    },
+    7 : {
+        name : 'launchpad ufobyrinth',
+        image : 'images/launchpad.webp',
+        ID : 127331837,
+        creator : 'ItsNoobe',
+        verifier : 'ItsNoobe',
+        recorder : 'ItsNoobe',
+        time : '2.712'
+    },
+    8 : {
         name : 'Fever',
-        image : 'Images/fever.webp',
+        image : 'images/fever.webp',
         ID : 114963741,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '17.400'
     },
-    6: {
+    9 : {
         name : 'Suffer',
-        image : 'Images/suffer.webp',
+        image : 'images/suffer.webp',
         ID : 111863467,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '21.912'
     },
-    7: {
+    10 : {
         name : 'agony',
-        image : 'Images/agony.webp',
+        image : 'images/agony.webp',
         ID : 114960048,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '19.166'
     },
-    8: {
+    11 : {
         name : 'mangoa',
-        image : 'Images/mangoa.webp',
+        image : 'images/mangoa.webp',
         ID : 114952534,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'MyCat223',
         time : '15.812'
     },
-    9: {
-        name : 'vsc but ufo',
-        image : 'Images/vscUfo.webp',
+    12 : {
+        name : 'VSC but ufo',
+        image : 'images/vscUfo.webp',
         ID : 114984854,
         creator : 'CoinvesselGD',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '35.791'
     },
-    10: {
+    13 : {
         name : 'Mycat sigma boy chal',
-        image : 'Images/sigmaBoy.webp',
+        image : 'images/sigmaBoy.webp',
         ID : 114984412,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '17.754'
     },
-    11: {
+    14 : {
         name : 'spam challenge 1',
-        image : 'Images/spam1.webp',
+        image : 'images/spam1.webp',
         ID : 114952847,
         creator : 'tenkuyuhh',
         verifier : 'tenkuyuhh',
         recorder : 'tenkuyuhh',
         time : '20.091'
     },
-    12: {
+    15 : {
+        name : 'RENEWAL',
+        image : 'images/renewal.webp',
+        ID : 118612227,
+        creator : 'ItsNoobe',
+        verifier : 'ItsNoobe',
+        recorder : 'ItsNoobe',
+        time : '29.641'
+    },
+    16 : {
+        name : 'UFO Funk',
+        image : 'images/ufoFunk.webp',
+        ID : 124325607,
+        creator : 'CoinvesselGD',
+        verifier : 'Astatine16',
+        recorder : 'Astatine16',
+        time : '14.000'
+    },
+    17 : {
         name : 'average udufopcl level',
-        image : 'Images/average.webp',
+        image : 'images/average.webp',
         ID : 114935113,
         creator : 'MyCat223',
         verifier : 'MyCat223',
         recorder : 'MyCat223',
         time : '37.762'
     },
-    13: {
+    18 : {
         name : 'zimbabwe',
-        image : 'Images/zimbabwe.webp',
+        image : 'images/zimbabwe.webp',
         ID : 115127631,
         creator : 'TheNewCodex',
         verifier : 'TheNewCodex',
         recorder : 'TheNewCodex',
         time : '9.166'
     },
-    14: {
+    19 : {
+        name : 'TOOMDUDUFOIGDPCLBM',
+        image : 'images/toomdud.webp',
+        ID : 114955327,
+        creator : 'Codex',
+        verifier : 'Codex',
+        recorder : 'Codex',
+        time : '9.083'
+    },
+    20 : {
         name : 'le funky ufo level',
-        image : 'Images/leFunky.webp',
+        image : 'images/leFunky.webp',
         ID : 114952921,
         creator : 'ItsNoobe',
         verifier : 'ItsNoobe',
         recorder : 'rush',
         time : '29.770'
     },
-    15: {
-        name : 'TOOMDUDUFOIGDPCLBM',
-        image : 'Images/toomdud.webp',
-        ID : 114955327,
-        creator : 'Codex',
-        verifier : 'Codex',
-        recorder : 'Codex',
-        time : '9.083'
-    }
 };
